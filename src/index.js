d60c51a9f77195a61d63d800fa0068148f297f7b
FrodeBerg <bergstedt.frode@gmail.com>
2023-02-25 22:45:44 +0100

    Add click event with cordinates

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-import "./style.css"
+import "./style.css";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const page = document.getElementById("page");
+  page.addEventListener("click", (event) => {
+
+    // Prompt info
+    // Create either task or project 
+
+  });
+});
+
+const tasks = [];
+const projects = [];
+
+const task = (title, x, y) => {
+  return {
+    x,
+    y,
+    title,
+  };
+};
+
+// Load data on login
+
+// Nodes
+
+// Each node is a project or a task
+
+// Nodes can have colours, shapes
+
+// Tasks can be completed or not
+
+// Projects have percentages corresponding to the amount of finished tasks
+
+// Projects can be linked 100% = finished
+
+// Hover over task for information
+
+// Add logic for projects
